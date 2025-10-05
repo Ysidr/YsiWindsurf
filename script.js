0934af7009de2f1e17e4a5080707b7ebc9af2cc9
Ysidro <39842830+Ysidr@users.noreply.github.com>
2025-10-06 02:13:14 +0300

    big scale images and good grid

--- script.js
+++ script.js
@@ -4,21 +4,72 @@ const imgList = [
     "assets/img/IMG_2332.JPG"
 ];
 
-function createImgList(){
-    
-    chooseImgs();
-}
+const allImgList = [
+    "assets/img/IMG_6519_Original.JPG",
+    "assets/img/IMG_1839.JPG",
+    "assets/img/IMG_1898.JPG",
+    "assets/img/IMG_2149.JPG",
+    "assets/img/IMG_2332.JPG",
+    "assets/img/IMG_2615.JPG",
+    "assets/img/IMG_2670.JPG",
+    "assets/img/IMG_4971.JPG",
+    "assets/img/IMG_4972.JPG",
+    "assets/img/IMG_4973.JPG",
+    "assets/img/IMG_4974.JPG",
+    "assets/img/IMG_4975.JPG",
+    "assets/img/IMG_4976.JPG",
+    "assets/img/IMG_4977.JPG",
+    "assets/img/IMG_4978.JPG",
+    "assets/img/IMG_7036.JPG",
+    "assets/img/IMG_7037.JPG",
+    "assets/img/IMG_7038.JPG",
+    "assets/img/IMG_7039.JPG",
+    "assets/img/IMG_7040.JPG",
+    "assets/img/IMG_7041.JPG",
+    "assets/img/IMG_7042.JPG",
+    "assets/img/IMG_7043.JPG",
+    "assets/img/IMG_7044.JPG",
+    "assets/img/IMG_7045.JPG",
+    "assets/img/IMG_7045.JPG"
+];
 
-function chooseImgs(){
-   let x =  Math.floor(Math.random() * imgList.length);
-   renderImage(x);
+function init(){
+    createImgList();
+    renderAllImages();
 };
 
-function rotateImg(index){
-   if(index >= imgList.length){
-       index = 0;
-   }else if(index < 0){
-       index = imgList.length - 1;
-   }
-   renderImage(index);
-}
+function addDNone(id){
+    document.getElementById(id).classList.add("d-none")
+};
+
+
+function createImgList() {
+
+    chooseImgs();
+};
+
+function chooseImgs() {
+    let x = Math.floor(Math.random() * imgList.length);
+    renderImage(x);
+};
+
+function rotateImg(index) {
+    if (index >= imgList.length) {
+        index = 0;
+    } else if (index < 0) {
+        index = imgList.length - 1;
+    }
+    renderImage(index);
+    console.log("./assets");
+
+};
+
+function openImg(imageIndex) {
+    document.getElementById("greyOut").classList.toggle("d-none");
+    document.getElementById("greyOut").innerHTML = ` <div class="innerGrayOut" id="innerGrayOut">
+            <img src="${allImgList[imageIndex]}" alt="">
+        </div>`
+
+
+};
+
--- scripts/templates.js
+++ scripts/templates.js
@@ -3,3 +3,12 @@ function renderImage(index){
             <div class= "AboutMeRenderedImg"><img src="${imgList[index]}" alt="About Me Image" class="img-fluid rounded" style="max-width: 100%;"></div>
             <button onclick="rotateImg(${index+1})">-></button>`;
 };
+
+function renderAllImages(){
+    document.getElementById("gallery").innerHTML = ``;
+    let index=0;
+    allImgList.forEach(element => {
+        document.getElementById("gallery").innerHTML += `<img onclick="openImg(${index})" src="${element}" alt="Gallery Image">`
+        index++;
+    });
+}
